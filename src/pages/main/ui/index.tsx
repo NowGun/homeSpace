0a956d11b68bae38defe51590e6694a52d186be7
Daniil Zhirov <nowgun98@gmail.com>
2024-04-24 22:56:26 +0300

    тест для деплоя

--- src/pages/main/ui/index.tsx
+++ src/pages/main/ui/index.tsx
@@ -3,6 +3,8 @@ import {Time, Weather} from "@/widgets"
 
 const Main = () => {
 
+    console.log(import.meta.env.VITE_API_WEATHER_PATH, import.meta.env.VITE_API_WEATHER_ICON_PATH)
+
     return (
         <div className={styles.main}>
             <div className={styles.image}></div>
